c20d2dee1679c60850c33d4b72f1c7db83eaa865
Hassan Rehan <memonmuhamadhasan@gmail.com>
2022-02-01 22:59:34 +0500

    imported data, and read it in APP file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './bootstrap.min.css'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap'
-
+import products from './products';
 
 function App() {
   // const logo = require('./logo.svg') as string;
@@ -11,7 +11,9 @@ function App() {
     <div className="App py-3">
       <Header />
       <Container>
-
+        {products.map((product) => {
+          return (<div>{product.name} <br /></div>);
+        })}
         Hello World
         <Button className="btn btn-block">Buttons</Button>
 
