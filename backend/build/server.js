05953c8f6b0208d1c078e071c86a697655b6df96
Hassan Rehan <memonmuhamadhasan@gmail.com>
2022-02-03 23:09:35 +0500

    Set up Product api

--- backend/build/server.js
+++ backend/build/server.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
     res.send('Api is running');
 });
+app.use(productRoutes_1.default);
+// app.get('/api/products', (req, res) => {
+// })
 app.listen(5000, () => {
     console.log('api is running ');
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import prodcutRoutes from './routes/productRoutes'
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Api is running')
 })
+
+app.use(prodcutRoutes)
+// app.get('/api/products', (req, res) => {
+
+// })
 
 app.listen(5000, () => {
     console.log('api is running ')
