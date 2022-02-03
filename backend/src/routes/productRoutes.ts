2eda0124ddb247b1a8b041b741ed4d5858b43edc
Hassan Rehan <memonmuhamadhasan@gmail.com>
2022-02-03 23:09:35 +0500

    Set up Product api

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,4 +1,14 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 import products from '../data/products'
+
+const router = express.Router();
+
+router.get('/api/products', asyncHandler(async (req, res) => {
+    res.send(products)
+}))
+
+export default router
+
 
 
