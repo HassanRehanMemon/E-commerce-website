422506f5a5371f93a064d6412bda1a1cad054f7a
Hassan Rehan <memonmuhamadhasan@gmail.com>
2022-02-04 02:27:59 +0500

    redux error fixed

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
