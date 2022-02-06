1639e28269285719cef472130dd031069b75fd29
Hassan Rehan <memonmuhamadhasan@gmail.com>
2022-02-07 01:20:09 +0500

    register new user

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -11,7 +11,7 @@ export const authUser = expressAsyncHandler(async (req: Request, res: Response) 
 
     const user = await User.findOne({ email })
     if (user && await bcrypt.compare(password, user.password)) {
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
